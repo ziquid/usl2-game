10156f3dbc3cdc624391cd55e49d1141e1a6e306
Joseph Cheek <joseph@gdgwebworks.com>
2016-09-28 20:13:08 -0400

    add error page support

---
+++ sites/all/modules/custom/usl2/templates/pages/error.tpl.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @file
+ */
+?>
+<div class="welcome">
+  <div class="elder-image">
+  </div>
+  <p>
+    A wizened old man comes up to you.&nbsp; You recognize him as one of the
+    elders of the city.
+  </p>
+  <h2>
+    &ldquo;I'm so embarrassed.&rdquo;
+  </h2>
+  <h3>
+    Error <?php echo $error_code; ?>
+  </h3>
+  <h3>
+    User ID <?php echo $game_id; ?>
+  </h3>
+</div>
