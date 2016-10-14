dcf365632ce723a50a2db6972f3f378484a75c2a
Joseph Cheek <joseph@gdgwebworks.com>
2016-10-14 17:02:57 -0400

    finish (?) choosing a party!

--- sites/all/modules/custom/usl2/templates/pages/choose_party.tpl.php
+++ sites/all/modules/custom/usl2/templates/pages/choose_party.tpl.php
@@ -12,6 +12,7 @@
   $arg0 = check_plain(arg(0));
   $arg2 = check_plain(arg(2));
   include drupal_get_path('module', 'usl2') . '/inc/common/game_defs.inc';
+  $first_time = (boolean) $first_time;
 
   db_set_active('game');
 /*
@@ -39,27 +40,45 @@
     You are met by the city elder again.
   </p>
   <h2>
-    &ldquo;Well done.
+    <?php if ($first_time): ?>
+      &ldquo;Well done.
+    <?php else: ?>
+      &ldquo;Hello again.
+    <?php endif; ?>
   </h2>
-  <p>
-    &ldquo;I am impressed by what you have learned.
-  </p>
-  <p>&ldquo;In order to continue your journey, you will need a
-    mentor.&nbsp; Your mentor will provide guidance and answer any questions
-    that you may have.&nbsp; He or she should have provided you with a
-    referral code.
-  </p>
-  <p>
-    &ldquo;Alternatively, you can continue on your own without a
-    code.&nbsp; Which do you prefer?&rdquo;
-  </p>
-</div>
+  <?php if ($first_time): ?>
+    <p>
+      &ldquo;I am impressed by what you have learned.
+    </p>
+    <p>
+      &ldquo;In order to continue your journey, you will need a
+      mentor.&nbsp; Your mentor will provide guidance and answer any questions
+      that you may have.&nbsp; He or she should have provided you with a
+      referral code.
+    </p>
+    <p>
+      &ldquo;Alternatively, you can continue on your own without a
+      code.&nbsp; Which do you prefer?&rdquo;
+    </p>
+  <?php else: ?>
+    <p style="min-height: 88px;">
+      <?php echo t('&ldquo;So you wish to join a different @party.&nbsp; You will not rank as highly in that @party_small as you do in your current one, but that is your choice.&rdquo;', $txt); ?>
+    </p>
+  <?php endif; ?>
 
-<?php echo theme('game_button', array('link' => 'enter_referral_code', 'type' => 'I Have A Referral Code')); ?>
 
-<div class="welcome">
+<?php if ($first_time): ?>
+  </div>
+    <?php echo theme('game_button', array('link' => 'enter_referral_code', 'type' => 'I Have A Referral Code')); ?>
+  <div class="welcome">
+<?php endif; ?>
+
   <h3>
-    <?php echo t("If you don't have a referral code, choose a @party:", $txt); ?>
+    <?php if ($first_time): ?>
+      <?php echo t("If you don't have a referral code, choose a @party:", $txt); ?>
+    <?php else: ?>
+      Which one do you prefer?
+    <?php endif; ?>
   </h3>
 
   <?php foreach($data as $item): ?>
